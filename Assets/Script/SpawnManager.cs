b22e1d2c34a3c3085ea4e5a8a435cce3a13e328c
AxetheRoad <AxetheRoad@hotmail.com>
2022-03-06 21:56:03 -0800

    Lesson D

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -9,23 +9,24 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3.0f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            float RanXPos = Random.Range(-xPositionRange, xPositionRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 RanPos = new Vector3(RanXPos, 0, 21);
-            Instantiate(animalPrefabs[animalPrefabIndex], RanPos, 
-                animalPrefabs[animalPrefabIndex].transform.rotation);
+               
 
+    }
 
-        }
+    void SpawnRandomAnimal()
+    {
+        float RanXPos = Random.Range(-xPositionRange, xPositionRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 RanPos = new Vector3(RanXPos, 0, 21);
+        Instantiate(animalPrefabs[animalPrefabIndex], RanPos,
+            animalPrefabs[animalPrefabIndex].transform.rotation);
+
     }
 }
